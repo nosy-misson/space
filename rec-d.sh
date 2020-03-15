d92ca8d61cb5b5a7ed44464742700878063a3ca8
leka <leka@localhost.localdomain>
2020-03-15 16:38:04 +0100

    my first script

---
+++ rec-d.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+# this script removes spaces on file and repo. 
+rec(){
+	dir="$1/*"
+	for a in ${dir} ; do
+		b=$(echo $a | sed -e 's/[[:blank:]]/_/g');
+		
+	   	if [ -e "$b" ];then
+			echo "">/dev/null;
+		else
+			mv "$a" "$b";
+		fi
+		if  [  -d $b ] ;  then
+			rec $b
+		fi
+	done
+}
+
+rec "./$1"	
